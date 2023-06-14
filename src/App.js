a7d71eb3bb3975469463095cfb98c1fb6f4f60e7
thumbimurali <33730398+thumbimurali@users.noreply.github.com>
2023-06-14 13:12:59 +0530

    [commitHash:8e8f5fc]

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import TodoItem from './remote/new_todoItem'
+
+export default function App() {
+  return <TodoItem />
+}
